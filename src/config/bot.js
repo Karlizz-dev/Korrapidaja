ad850ee910226e49cb9207b2922d44af509c0588
Karlizz-dev <kangurkarlisorm@gmail.com>
2026-07-22 15:18:58 +0300

    Update bot activity status and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Playing", // required by Discord API, not shown in the client
-        state: "Actual Roleplay",     // this is what people actually see
-        type: 0,               // Custom
+        name: "Custom Status", // required by Discord API, not shown in the client
+        state: "🇪🇪 Eesti eest surmani!",     // this is what people actually see
+        type: 4,               // Custom
       },
     ],
   },
